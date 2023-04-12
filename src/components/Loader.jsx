d30aad0b4fad312a488e0d6af91678ced2efa6d9
wilbert <wilbertgescobar@gmail.com>
2023-04-12 02:45:35 -0500

    will

---
+++ src/components/Loader.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Loader.css'
+
+const loader = () => {
+  return (
+    <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+  )
+}
+
+export default loader
